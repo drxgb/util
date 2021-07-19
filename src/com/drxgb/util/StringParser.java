8178f76b9fd4a3eb9e4bad85a8b5e8b8afed26e2
Dr.XGB <dr.xgb.rm2k@gmail.com>
2021-07-19 20:41:30 -0300

    Update: StringParser

--- src/com/drxgb/util/StringParser.java
+++ src/com/drxgb/util/StringParser.java
@@ -1,7 +1,8 @@
 package com.drxgb.util;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.Reader;
 
 /**
  * Classe abstrata responsável por receber e tratar dados para
@@ -15,11 +16,11 @@ public abstract class StringParser
 	 * <p>Lê os dados de um arquivo e o transforma em string.</p>
 	 * <p>A leitura é realizada em uma tripa de bytes e se encerra até
 	 * chegar ao fim do arquivo ou quando encontrar um caractere nulo (0x00).</p>
-	 * @param reader Instância de {@code InputStreamReader}.
+	 * @param reader Instância de {@code Reader}.
 	 * @return O resultado da leitura do arquivo.
 	 * @throws IOException
 	 */
-	public static String fromInputStreamReader(InputStreamReader reader) throws IOException
+	public static String fromReader(Reader reader) throws IOException
 	{
 		String str = "";
 		int ch = 0;
@@ -29,6 +30,41 @@ public abstract class StringParser
 			if (ch != 0)
 				str += (char) ch;
 		} while (ch != 0 && ch != -1);
+		return str;
+	}
+	
+	/**
+	 * <p>Lê os dados de um arquivo e o transforma em string.</p>
+	 * <p>A leitura é realizada em uma tripa de bytes e se encerra até
+	 * chegar ao fim do arquivo ou quando encontrar um caractere nulo (0x00).</p>
+	 * @param reader Instância de {@code InputStream}.
+	 * @return O resultado da leitura do arquivo.
+	 * @throws IOException
+	 */
+	public static String fromInputStream(InputStream is) throws IOException
+	{
+		String str = "";
+		int ch = 0;
+		do
+		{
+			ch = is.read();
+			if (ch != 0)
+				str += (char) ch;
+		} while (ch != 0 && ch != -1);
+		return str;
+	}
+	
+	
+	/**
+	 * Lê os dados de um arranjo de caracteres e gera uma string.
+	 * @param ch Cadeia de caracteres.
+	 * @return O conetúdo da cadeira de caracteres em um tipo {@code String}
+	 */
+	public static String fromCharArray(char[] ch)
+	{
+		String str = "";
+		for (char c : ch)
+			str += c;
 		return str;
 	}
 }
