18ce079f6432b439739c51b74cb401ec4852ce24
Dr.XGB <dr.xgb.rm2k@gmail.com>
2021-07-24 09:21:57 -0300

    Update: StringFormatter

--- src/com/drxgb/util/StringFormatter.java
+++ src/com/drxgb/util/StringFormatter.java
@@ -1,7 +1,20 @@
 package com.drxgb.util;
 
+/**
+ * Classe abstrata responsável por tratar strings e fazer
+ * suas devidas conversões
+ * @author Dr.XGB
+ * @version 1.8
+ */
 public abstract class StringFormatter
 {
+	/**
+	 * <p>Transforma um string, substituindo a primeira letra
+	 * por uma maiúscula.</p>
+	 * <p>Exemplo: <em>tomate</em> vira <em>Tomate</em>.</p>
+	 * @param str A string a ser transformada.
+	 * @return Resultado da transformação.
+	 */
 	public static String capitalize(String str)
 	{
 		if (str.isEmpty() || str.isBlank() || str == null)
