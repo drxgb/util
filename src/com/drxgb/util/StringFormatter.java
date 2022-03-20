d4c8a503f8ec081b74f143d17dc68fe7073e36a0
Dr.XGB <dr.xgb.rm2k@gmail.com>
2022-03-20 16:36:04 -0300

    Stringformatter.minify() added

--- src/com/drxgb/util/StringFormatter.java
+++ src/com/drxgb/util/StringFormatter.java
@@ -4,7 +4,7 @@ package com.drxgb.util;
  * Classe abstrata responsável por tratar strings e fazer
  * suas devidas conversões
  * @author Dr.XGB
- * @version 1.8
+ * @version 1.8.1
  */
 public abstract class StringFormatter
 {
@@ -12,7 +12,7 @@ public abstract class StringFormatter
 	 * <p>Transforma um string, substituindo a primeira letra
 	 * por uma maiúscula.</p>
 	 * <p>Exemplo: <em>tomate</em> vira <em>Tomate</em>.</p>
-	 * @param str A string a ser transformada.
+	 * @param str String a ser transformada.
 	 * @return Resultado da transformação.
 	 */
 	public static String capitalize(String str)
@@ -22,5 +22,27 @@ public abstract class StringFormatter
 		
 		String s = ("" + str.charAt(0)).toUpperCase();
 		return s + str.substring(1);
+	}
+	
+	
+	/**
+	 * <p>Retira quaisquer espaços em branco e quebras de linha.</p>
+	 * <p><strong>Exemplo:</strong> <em>Alegria do Brasil</em> vira <em>AlegriadoBrasil</em></p>
+	 * @param str String para ser minificada.
+	 * @return Resultado da minificação.
+	 */
+	public static String minify(String str)
+	{
+		if (str.isEmpty() || str.isBlank() || str == null)
+			return str;
+		
+		String out = "";
+		for (int i = 0; i < str.length(); ++i)
+		{
+			char c = str.charAt(i);
+			if (c != 0x0A && c != 0x0D && c != 0x20)
+				out += c;
+		}
+		return out;
 	}
 }
